f160101edc1b8cceca2756664865a66b1a805f3e
mykytaoleksenko <40619913@live.napier.ac.uk>
2023-04-26 00:02:41 +0100

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal(".menu_type_div", {delay: 250}, {easing: 'ease-in-out'}, {duration: 500});
+ScrollReveal().reveal(".button_2block_1", {delay: 250}, {easing: 'ease-in-out'}, {duration: 500});
+ScrollReveal().reveal(".button_2block_2", {delay: 250}, {easing: 'ease-in-out'}, {duration: 500});
+ScrollReveal().reveal(".mothers_day_ph", {delay: 250}, {easing: 'ease-in-out'}, {duration: 500});
+ScrollReveal().reveal("h2", {delay: 250}, {easing: 'ease-in-out'}, {duration: 500});
+ScrollReveal().reveal(".mothers_day_p", {delay: 250}, {easing: 'ease-in-out'}, {duration: 500});
+ScrollReveal().reveal(".button_3block_1", {delay: 250}, {easing: 'ease-in-out'}, {duration: 500});
+ScrollReveal().reveal(".art_within_mixology_p", {delay: 250}, {easing: 'ease-in-out'}, {duration: 500});
+ScrollReveal().reveal("h2", {delay: 250}, {easing: 'ease-in-out'}, {duration: 500});
+ScrollReveal().reveal(".art_within_mixology_ph", {delay: 250}, {easing: 'ease-in-out'}, {duration: 500});
+ScrollReveal().reveal(".button_3block_2", {delay: 250}, {easing: 'ease-in-out'}, {duration: 500});
